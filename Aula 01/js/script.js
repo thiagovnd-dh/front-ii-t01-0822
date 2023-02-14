55666d47f23071067a8a0b1bd9d69906bec50f26
Thiago Valadares Noleto Damasceno <87128753+thiagovnd-dh@users.noreply.github.com>
2023-02-14 16:38:13 -0500

    Aula 01 v01

---
+++ Aula 01/js/script.js
@@ -0,0 +1,9 @@
+let listaNumeros = [1, 2, 4, 8];
+
+// let i = 0;
+// let soma = 0;
+// while(i < listaNumeros.length){
+//     soma = soma + listaNumeros[i];
+//     console.log(soma);
+//     i++;
+// }
