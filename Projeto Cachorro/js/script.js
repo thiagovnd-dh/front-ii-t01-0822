be8750ea5074f685ebf950c37ed5314405607652
Thiago Valadares Noleto Damasceno <87128753+thiagovnd-dh@users.noreply.github.com>
2023-03-27 18:46:02 -0400

    Parte 1 Projeto Cachorro
    Aula 15

---
+++ Projeto Cachorro/js/script.js
@@ -0,0 +1,37 @@
+//https://dog.ceo/api/breeds/list/all
+
+
+//Maneira antiga de se fazer requisição Assíncrona
+// fetch("https://dog.ceo/api/breeds/list/all").then(function (response) {
+//     return response.json();
+// }).then(function(data) {
+//     console.log(data);
+// })
+
+async function puxarDados() {
+    const resposta = await fetch("https://dog.ceo/api/breeds/list/all");
+    const dados = await resposta.json();
+    console.log(dados);
+    criarListaRacas(dados.message);
+}
+
+puxarDados();
+
+function criarListaRacas(listaRacas) {
+    document.getElementById("racas").innerHTML = `
+        <select onchange = "carregarPorRaca(this.value)">
+            <option>Escolha um cachorro ou raça</option>
+            ${Object.keys(listaRacas).map(function (raca) {
+                return `<option>${raca}</option>`
+            }).join('')}
+        </select>
+    `;
+}
+
+function carregarPorRaca(raca) {
+    if(raca != "Escolha um cachorro ou raça") {
+        console.log(`Escolheu ${raca}`);
+    } else {
+        return;
+    }
+}
