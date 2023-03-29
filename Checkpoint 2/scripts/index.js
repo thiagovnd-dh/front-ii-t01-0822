f35a3a96b1881c54a44fb4139fc834ec26db97f5
Thiago Valadares Noleto Damasceno <87128753+thiagovnd-dh@users.noreply.github.com>
2023-03-29 18:24:35 -0400

    Requisições da API DH

---
+++ Checkpoint 2/scripts/index.js
@@ -0,0 +1,28 @@
+let loginUsuario = {
+    email: "thiagovnd@gmail.com",
+    password: "123456789"
+}
+
+const loginUsuarioJson = JSON.stringify(loginUsuario);
+
+let configuracoesRequisicao = {
+    method: 'POST',
+    body: loginUsuarioJson,
+    headers: {
+        'Content-type': 'application/json',
+    },
+}
+
+// fetch("https://todo-api.ctd.academy/v1/users/login", configuracoesRequisicao) //1
+//   .then((response) => response.json()) //2
+//   .then((user) => {
+//     console.log(user.jwt); //3
+//   });
+
+async function fazerLogin() {
+    const resposta = await fetch(`https://todo-api.ctd.academy/v1/users/login`, configuracoesRequisicao);
+    let chaveJwt = await resposta.json();
+    console.log(chaveJwt.jwt);
+}
+
+fazerLogin();
