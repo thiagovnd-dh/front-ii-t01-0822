ff74ca6057b12cf8d42a1a7997d96ae39293709b
Thiago Valadares Noleto Damasceno <87128753+thiagovnd-dh@users.noreply.github.com>
2023-04-06 21:13:39 -0400

    Finalizando funcionalidades das tarefas

--- Checkpoint 2/scripts/tarefas/tarefas.js
+++ Checkpoint 2/scripts/tarefas/tarefas.js
@@ -1,4 +1,6 @@
 let jwt;
+let tarefasPendentesDom = document.querySelector(".tarefas-pendentes");
+let tarefasTerminadas = document.querySelector(".tarefas-terminadas");
 
 onload = () => {
     jwt = sessionStorage.getItem("jwt");
@@ -82,33 +84,50 @@ let listaTarefasGlobal = [];
 
 function rederizaTarefas(listaTarefas) {
     listaTarefasGlobal = listaTarefas;
-    
-    let tarefasPendentesDom = document.querySelector(".tarefas-pendentes");
 
     for(let tarefa of listaTarefas) {
+        let dataTarefa = new Date(tarefa.createdAt).toLocaleDateString("pt-BR");
+        let horaTarefa = new Date(tarefa.createdAt).toLocaleTimeString("pt-BR");
         if(!tarefa.completed) {
             //Tarefas Pendentes
             let li = document.createElement('li');
             li.classList.add("tarefa");
+            li.id = `id-${tarefa.id}`;
             li.innerHTML = `
-                <div class="not-done"></div>
+                <div class="not-done" onclick="completarTarefa(${tarefa.id})"></div>
                     <div class="descricao">
                     <p class="nome">${tarefa.description}</p>
-                    <p class="timestamp">${tarefa.createdAt}</p>
+                    <p class="timestamp">
+                        <i class="far fa-calendar-alt"></i> ${dataTarefa}
+                        <i class="far fa-clock"></i> ${horaTarefa}
+                    </p>
                 </div>
             `;
             tarefasPendentesDom.appendChild(li);
         }
         else{
             //Tarefas Completas
+            let li = document.createElement('li');
+            li.classList.add("tarefa");
+            li.id = `id-${tarefa.id}`;
+            li.innerHTML = `
+                    <div class="descricao">
+                    <p class="nome">${tarefa.description}</p>
+                    <div class="opcoes-tarefas-completas">
+                        <button><i id="tarefa_${tarefa.id}" class="fas fa-undo-alt change" title="Voltar para tarefa pendente"></i></button>
+                        <button><i id="tarefa_${tarefa.id}" class="far fa-trash-alt"></i></button>
+                    </div>
+                </div>
+            `;
+            tarefasTerminadas.appendChild(li);
         }
     }
 }
 
 // Cadastra nova tarefa
-let botaoCadastrar = document.getElementById('botaoCriarTarefa');
+let formNovaTarefa = document.querySelector('.nova-tarefa');
 
-botaoCadastrar.addEventListener('click', evento => {
+formNovaTarefa.addEventListener('submit', evento => {
     evento.preventDefault();
     let descricaoTarefa = document.getElementById('novaTarefa');
     if(descricaoTarefa.value != "") {
@@ -138,12 +157,71 @@ botaoCadastrar.addEventListener('click', evento => {
             adicionarNovaTarefaDom(dados);
         });
         //Adicionar na DOM
-        descricaoTarefa.value = "";
+        
     } else {
         alert('Escreva a descrição da tarefa');
     }
 });
 
 function adicionarNovaTarefaDom(tarefa) {
-    console.log(tarefa.description);
+    // console.log(tarefa.description);
+    // console.log(tarefa.createdAt);
+    let novaTarefa = document.createElement('li');
+    let dataTarefa = new Date(tarefa.createdAt).toLocaleDateString("pt-BR");
+    let horaTarefa = new Date(tarefa.createdAt).toLocaleTimeString("pt-BR");
+    novaTarefa.classList.add('tarefa');
+    novaTarefa.id = `id-${tarefa.id}`;
+    novaTarefa.innerHTML = `
+                <div class="not-done" onclick="completarTarefa(${tarefa.id})"></div>
+                    <div class="descricao">
+                    <p class="nome">${tarefa.description}</p>
+                    <p class="timestamp">
+                        <i class="far fa-calendar-alt"></i> ${dataTarefa}
+                        <i class="far fa-clock"></i> ${horaTarefa}
+                    </p>
+                </div>
+            `;
+    tarefasPendentesDom.appendChild(novaTarefa);
+}
+
+//Completar a Tarefa
+function completarTarefa(tarefaId) {
+    corpoDaRequisicao = {
+        "completed": true
+    }
+    let configuracoesRequisicao = {
+        method: 'PUT',
+        body: JSON.stringify(corpoDaRequisicao),
+        headers: {
+            'Content-type': 'application/json',
+            'authorization' : jwt
+        },
+    }
+    fetch(`${apiBaseUrl()}/tasks/${tarefaId}`, configuracoesRequisicao)
+    .then(chamada => {
+        if(chamada.status == 201 || chamada.status == 200){
+            return chamada.json();
+        }
+        throw response;
+    }).then(dados => {
+        console.log(dados);
+        moveTarefaParaTerminada(dados);
+    });
+}
+
+function moveTarefaParaTerminada(tarefa){
+    let tarefaMovida = document.getElementById(`id-${tarefa.id}`);
+    tarefaMovida.remove();
+    let tarefaTerminada = document.createElement('li');
+    tarefaTerminada.classList.add('tarefa');
+    tarefaTerminada.innerHTML = `
+            <div class="descricao">
+                <p class="nome">${tarefa.description}</p>
+                <div class="opcoes-tarefas-completas">
+                    <button><i id="tarefa_${tarefa.id}" class="fas fa-undo-alt change" title="Voltar para tarefa pendente"></i></button>
+                    <button><i id="tarefa_${tarefa.id}" class="far fa-trash-alt"></i></button>
+                </div>
+            </div>
+        `;
+    tarefasTerminadas.appendChild(tarefaTerminada);
 }
